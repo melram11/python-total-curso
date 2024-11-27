a860562c0efab6a4f685e7d43cbe561ae59a58d4
melram11 <128021731+melram11@users.noreply.github.com>
2024-11-26 22:35:37 -0600

    agregando carpeta variables de dia 2

---
+++ dia 2/Variables.py
@@ -0,0 +1,22 @@
+nombre = "Juan"
+print(nombre)
+
+nombre = "Laura"
+print(nombre)
+edad = 30 
+print(edad)
+edad2 = 15
+print(edad + edad2)
+
+nombre = input("dime tu nombre")
+print(" tu nombre es " + nombre)
+nombre = 'Hola'
+nombre2 = 'python'
+frase =  nombre + " " + nombre2
+print(frase)
+edad = 30
+edad2 = 50 
+frase = edad + edad2
+
+curso = "python"
+print("estas estudiando un curso de", curso)
---
+++ dia 2/integer_floats.py
@@ -0,0 +1,20 @@
+mi_numero = 1 + 3
+print(mi_numero)
+print(mi_numero + mi_numero)
+print(type(mi_numero))
+
+mi_numero2 = 5.8
+print(mi_numero2)
+print(mi_numero2 + mi_numero2)
+print(type(mi_numero2))
+
+edad = input("dime tu edad: ")
+print("tu edad es " + edad)
+
+print(edad + edad)
+print(type(edad))
+
+num1 = 2.5
+num2 = 3.5
+print(num1 + num2)
+print(type(num1 + num2))
