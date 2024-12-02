9cb96aa1917b3930db16e9ac2a794780803721cb
melram11 <128021731+melram11@users.noreply.github.com>
2024-12-01 23:09:54 -0600

    aprendiendo formateo y conversiones

---
+++ dia 2/Conversiones.py
@@ -0,0 +1,33 @@
+num1 = 20
+num2 = 30.5
+
+num1 = num1 + num2 # variable inplicita porque python la hizo automaticamente
+
+print(num1)
+print(type(num1))
+print(type(num2))
+
+
+# conversion explicita
+
+num3 = 5.8
+print(num3)
+print(type(num3))
+
+num4 = int(num3) # conversion explicita
+print(num4)
+print(type(num4))
+
+edad = input("dime tu edad: ")
+print(type(edad))
+edad = int(edad) 
+
+print(type(edad))
+nueva_edad = 1 + edad
+print(nueva_edad)
+
+num5 = "7.5"
+num6 = "10"
+
+print(float(num6) + float(num5)) # concatenacion de strings
+print('Hola ' + input("Dime tu nombre:") + " " + input("dime tu apellido:") +  "\nBuenos dias!" )
---
+++ dia 2/formateo_De_cadenas.py
@@ -0,0 +1,26 @@
+x = 10
+y = 5
+print("Mis numeros son " + str(x) + " y " + str(y))
+print("Mis numeros son {} y {}".format(x, y))
+print("La suma de {} y {} es {}".format(x,y,x+y)) 
+
+color = "rojo"
+matricula = "ABC123"
+
+print(f"El auto es {color} y su matricula es {matricula}")
+
+#ejericios
+nombre_asociado = "Jesse Pinkman"
+numero_asociado = 399058
+
+print(f"Estimado/a {nombre_asociado}, su número de asociado es: {numero_asociado}")
+
+puntos_nuevos = 350
+puntos_totales = 1225
+
+print("Has ganado {} puntos! En total, acumulas {} puntos".format(puntos_nuevos,puntos_totales))
+
+puntos_anteriores = 875
+puntos_nuevos = 350
+
+print(f"Has ganado {puntos_nuevos} puntos! En total, acumulas {puntos_anteriores+puntos_nuevos} puntos")
